6e863f2af2927acb09b25f7469180ec66f94b1e7
Joseph Forsman <jrforsman@gmail.com>
2017-11-17 14:22:22 -0500

    added two new data wrapper classes
    to be used in the future. some architecting

---
+++ WebService/src/Match.java
@@ -0,0 +1,14 @@
+/*
+ * wrapper class to insert a match record into a tournament table
+ */
+public class Match {
+	public String winner_name;
+	public int winner_id;
+	public String loser_name;
+	public int loser_id;
+	public int loser_score;
+	public int winner_score;
+	
+	//?
+	public String round_title;
+}
---
+++ WebService/src/Tournament.java
@@ -0,0 +1,11 @@
+/*
+ * Class to model a tournament object stored in the database.
+ */
+public class Tournament {
+	//participants
+	//set ? Hash table? ??
+	
+	public int participantNumber;
+	public String name;
+	public String link;
+}
