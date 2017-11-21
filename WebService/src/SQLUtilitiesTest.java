534e51ac7235535b91f242e659dbb0fec838a875
Joseph Forsman <jrforsman@gmail.com>
2017-11-21 01:10:21 -0500

    Added simple test class to be able to run inserts and such individually
    Doesn't actually test much as of now, just makes these functions easy to call (and then validate against the mysql database client)

---
+++ WebService/src/SQLUtilitiesTest.java
@@ -0,0 +1,32 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SQLUtilitiesTest {
+
+	@Test
+	public void testGetLastCheckedDate() {
+		SQLUtilities util = new SQLUtilities();
+		
+		String temp = util.getLastCheckedDate();
+		
+		assertEquals(temp, "2000-01-03");
+	}
+
+	@Test
+	public void testInsertTournament() {
+		SQLUtilities util = new SQLUtilities();
+		
+		Tournament x = new Tournament();
+		
+		x.name = "test2";
+		x.link = "challonge.com";
+		x.dateStarted = "2000-01-03";
+		x.id = 5003;
+		
+		util.insertTournament(x);
+		
+		assertTrue(true);
+	}
+	
+}
