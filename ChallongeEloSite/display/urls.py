9ea3287360d8199ae2a76d8fc15263bf60037735
Joseph Forsman <jrforsman@gmail.com>
2017-12-06 14:36:25 -0500

    added players and basic tournament page
    reworked individual player location name

--- ChallongeEloSite/display/urls.py
+++ ChallongeEloSite/display/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('index.html', views.index, name='index'),
     path('rankings.html', views.rankings, name='rankings'),
     path('doubles.html', views.doubles_rankings, name='doubles'),
+    path('players.html', views.players_page, name='players_page'),
+    path('players/<int:player_id>/', views.player_detail, name='player_detail'),
+    path('tournaments.html', views.tournament_page, name='tournaments'),
 ]
--- ChallongeEloSite/display/views.py
+++ ChallongeEloSite/display/views.py
@@ -24,3 +24,22 @@ def doubles_rankings(request):
 
 	#get only the latest date, not date and time
 	return render(request, 'display/doubles.html', {'lastDate': line, 'players': players.objects.filter(name__contains=" and ").order_by('-elo')})
+
+def players_page(request):
+	#get latest time
+	line = str(tournaments.objects.latest(field_name='date_started').date_started)
+	line = line[:10]
+
+	#get only the latest date, not date and time
+	return render(request, 'display/players.html', {'lastDate': line, 'players': players.objects.all().order_by('-elo')})
+
+def player_detail(request):
+	return HttpResponse('Constrct')
+
+def tournament_page(request):
+	#get latest time
+	line = str(tournaments.objects.latest(field_name='date_started').date_started)
+	line = line[:10]
+
+	#get only the latest date, not date and time
+	return render(request, 'display/tournaments.html', {'lastDate': line, 'tournaments': tournaments.objects.all().order_by('date_started')})
