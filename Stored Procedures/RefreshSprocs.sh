50d63b9b61a1c328e841be5982caa7c0339b3015
Joseph Forsman <jrforsman@gmail.com>
2018-01-14 14:57:45 -0500

    updated shell scripts to use loop

--- Stored Procedures/RefreshSprocs.sh
+++ Stored Procedures/RefreshSprocs.sh
@@ -1,25 +1,4 @@
 #!/bin/sh
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "GetIDFromName.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "GetLatestTourneyDate.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "GetNameFromAlias.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "GetPlayerID.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "InsertAlias.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "InsertPlayer.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "InsertTournament.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "InsertPlayerName.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "InsertPlacing.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "InsertMatch.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "GetPlayerName.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "GetElo.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "SetElo.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "GetEmptyPlayers.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "GetAliases.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "DeletePlayer.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "DeletePlacings.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "DeleteMatches.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "GetMatches.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "UpdatePlayerId.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "UpdateAliasReferences.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "InsertEventLog.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "GetCountOfMethod.sql"
-mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < "RefreshTables.sql"
+for file in *.sql; do
+  mysql -h "petlanddb.cv18qdrgjzn8.us-east-2.rds.amazonaws.com" -u "admin" "-ppassword" "challongepowell" < $file
+done
